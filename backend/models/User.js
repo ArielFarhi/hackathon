722299481cc664bedb674358056edbeedbc73518
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2025-01-03 11:30:02 +0200

    Add user model

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    password: { type: String },
+    image: { type: String },
+    points: { type: Number, default: 0 },
+}, { collection: "users" });
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
