eff48fc19c0fb4d4ece77a4cd6f8a0772b4040b4
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2025-01-03 12:25:22 +0200

    Add contants

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 const logger = require("morgan");
+
 const { authRouter } = require("./routers/authRouter");
-<<<<<<< Updated upstream
-const { lederbordRouter } = require("./routers/leaderboardRouter")
-=======
+const { leaderboardRouter } = require("./routers/leaderboardRouter")
 const { triviaRouter } = require("./routers/triviaRouters");
->>>>>>> Stashed changes
 
 const port = process.env.PORT || 8080;
 
@@ -14,12 +12,9 @@ app.use(express.json());
 app.use(logger("dev"));
 
 app.use("/auth", authRouter);
-<<<<<<< Updated upstream
-app.use("/leaderboard", lederbordRouter);
-=======
+app.use("/leaderboard", leaderboardRouter);
 app.use("/trivia", triviaRouter);
 
->>>>>>> Stashed changes
 
 app.use((req, res) => {
   res.status(404).send("Page wasn't found");
