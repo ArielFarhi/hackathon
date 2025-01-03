86cf7e11b7a0842edf7afb8811e7092b44ffe110
Novakovskiy Rodion <novakovskiy.ra@outlook.com>
2025-01-03 12:22:01 +0200

    Changed rout to leaderboard

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const logger = require("morgan");
 const { authRouter } = require("./routers/authRouter");
-const { lederbordRouter } = require("./routers/leaderboardRouter")
+const { leaderboardRouter } = require("./routers/leaderboardRouter")
 
 const port = process.env.PORT || 8080;
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(logger("dev"));
 
 app.use("/auth", authRouter);
-app.use("/leaderboard", lederbordRouter);
+app.use("/leaderboard", leaderboardRouter);
 
 app.use((req, res) => {
   res.status(404).send("Page wasn't found");
