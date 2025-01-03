6017da0f705d6e349e5650ad9b71fb64512fa40c
ShavitIdan <idanshavit5@gmail.com>
2025-01-03 11:37:36 +0200

    implemented auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,24 @@
+const authController = {
+  async register(req, res) {
+    try {
+      const { username, email, password } = req.body;
+      const image = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+      const user = await User.create({ username, email, password, image });
+      res.status(201).json({ user });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  async login(req, res) {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.status(200).json({ user });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+};
